7ecc456d8a46ddd350e1a0c55535b82e921af4fd
Nikhil Kondabala <codenamenikky@gmail.com>
2018-10-11 15:36:03 -0400

    this is going to be an interesting commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Choice of Energy. 
+
+## New post. 
+1. Add to the src/mdxposts folder the posts that you want to add. 
+2. publish the posts to the website. 
+
+> Credits:
+This is a edited template based on https://github.com/Ganevru/gatsby-starter-blog-grommet.git
---
+++ site-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  //
+  // Change information to your own:
+  //
+  // Site Info:
+  title: 'Choice of Energy',
+  description: 'This is a atempt to share knowledge about energy',
+  siteUrl: 'https://choiceofenergy.com',
+  footerText:
+    '**Choice of Energy**<br><br>trying to share energy knowledge', // You can use Markdown markup here
+
+  // Blog Author Info:
+  author: 'Nikhil Kondabala',
+  authorImage:
+
+    //'https://pbs.twimg.com/profile_images/986603715732934656/7A4Nt68k_400x400.jpg',
+    './src/componenets/biopic.jpg',
+  authorBio:
+    "Hello, I'm Nikhil Kondabala I work in the advanced energy economy. This is space where I try to explore open datasets about the energy and share the knowledge from them.", // You can use Markdown markup here
+  social: {
+    twitter: 'codenamenikky',
+    github: 'codenamenikky',
+    reddit: ''
+  },
+
+  // Services:
+  googleAnalyticsTrackingId: `ADD YOUR TRACKING ID HERE`,
+
+  // Site Style:
+  font: 'Roboto', // Roboto
+  fontHeadings: 'Merriweather', // Merriweather
+  brandColor: '#7eafab', // #7D4CDB
+  backgroundColor: '#fff' // #fff
+
+  // TODO:
+  // pathPrefix: null,
+  // logo: path.resolve(__dirname, 'src/images/icon.png')
+}
